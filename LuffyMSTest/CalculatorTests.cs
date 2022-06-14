b51ae6825a104e1680fbfc2380445afea0abc627
Deenadhayalan17 <deena17698ddd@gmail.com>
2022-06-14 12:30:19 +0530

    Added a Test Case

--- LuffyMSTest/CalculatorTests.cs
+++ LuffyMSTest/CalculatorTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Luffy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,20 @@ using System.Threading.Tasks;
 
 namespace LuffyMSTest
 {
+    [TestClass]
     class CalculatorTests
     {
+        [TestMethod]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            //Arrange
+
+            Calculator cal = new Calculator();
+
+            //Act
+            int result = cal.AddNumber(5, 4);
+            //Assert
+            Assert.AreEqual(9, result);
+        }
     }
 }
